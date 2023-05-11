0289e1e66513430e1f9b56fce00c007043002f39
johnwilly990 <j.kim1940@gmail.com>
2023-05-11 15:37:58 -0400

    Create knex and migrations file

---
+++ migrations/20230511191022_leagues.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("leagues", (table) => {
+    table.uuid("id").primary();
+    table.string("name").notNullable();
+    table.string("country").notNullable();
+    table.string("logo").notNullable();
+    table.string("country_flag").notNullable();
+    table.integer("coefficient").notNullable();
+    table.integer("founding_year").notNullable();
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable("leagues");
+};
