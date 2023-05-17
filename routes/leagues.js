4d57977c249f1241fd0bbb7ac45b2b43944b6078
johnwilly990 <j.kim1940@gmail.com>
2023-05-17 12:45:15 -0400

    Change GET endpoint to get all leagues

--- routes/leagues.js
+++ routes/leagues.js
@@ -7,12 +7,16 @@ const db = knex(knexConfig);
 // Get all leagues
 router.get("/", async (_req, res) => {
   try {
-    const leagues = await db("leagues").select("*");
+    const leagues = await db("leagues").select("id", "name", "logo");
 
-    console.log(leagues);
-  } catch (err) {
-    console.error(err)
+    if (leagues.length === 0) {
+      res.status(404).json({ message: "No leagues found" });
     }
+
+    res.status(200).send(leagues);
+  } catch (err) {
+    console.error(err);
+  }
 });
 
 module.exports = router;
